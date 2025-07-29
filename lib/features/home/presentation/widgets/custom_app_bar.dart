2436812af4c1756bd28da5f840c47399ac66dbd0
abdallah <abdallahtolba17@gmail.com>
2025-07-29 06:00:02 +0300

    green

--- lib/features/home/presentation/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/widgets/custom_app_bar.dart
@@ -55,7 +55,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         const SizedBox(width: 16),
       ],
       elevation: 2,
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.green,
       foregroundColor: Theme.of(context).colorScheme.onSurface,
     );
   }
